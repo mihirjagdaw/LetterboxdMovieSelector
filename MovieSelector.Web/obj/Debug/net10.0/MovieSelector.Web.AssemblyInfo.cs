efc46c08853b67180905806e2896d29420ad2a30
ST10449392 <162105261+ST10449392@users.noreply.github.com>
2026-04-09 14:03:10 +0200

    your message here

--- MovieSelector.Web/obj/Debug/net10.0/MovieSelector.Web.AssemblyInfo.cs
+++ MovieSelector.Web/obj/Debug/net10.0/MovieSelector.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieSelector.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1b85c15eeb82e197c7b6495cf6e2fd5204f5601")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieSelector.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieSelector.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
